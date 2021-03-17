47391ba7b2d2b229b806fff93f88915ee6e2800c
9045857 <9045857@mail.ru>
2021-03-17 11:20:46 +0700

    добавил проверку методов Скалярное произведение и  сложение

--- L2T3UnitTest/VectorTests.cs
+++ L2T3UnitTest/VectorTests.cs
@@ -20,9 +20,27 @@ namespace L2T3UnitTest
         [TestMethod]
         public void TestGetSize()
         {
-            var vector = new Vector(4, new[] {1.0,1.0, 3.0, 4.0 });
+            var vector = new Vector(4, new[] { 1.0, 1.0, 3.0, 4.0 });
 
             Assert.IsTrue(Math.Abs(vector.GetSize() - 4) < Epsilon, "Должно быть 4");
+        }
+
+        [TestMethod]
+        public void TestGetScalarProduct()
+        {
+            var vector1 = new Vector(2, new[] { 2.0, 2.0 });
+            var vector2 = new Vector(2, new[] { 3.0, 3.0 });
+
+            Assert.IsTrue(Math.Abs(Vector.GetScalarProduct(vector1, vector2) - 12) < Epsilon, "Должно быть 12");
+        }
+
+        [TestMethod]
+        public void TestAddVector()
+        {
+            var vector1 = new Vector(2, new[] { 3.0, 3.0 });
+            var vector2 = new Vector(2, new[] { 5.0, 4.0 });
+
+            Assert.AreEqual(new Vector(2, new[] { 8.0, 7.0 }), vector1.AddVector(vector2));
         }
     }
 }
