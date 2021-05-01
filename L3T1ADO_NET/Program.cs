7a8c84b81b561c8a2c92c3f92a6b05581d4eaeae
9045857 <9045857@mail.ru>
2021-05-01 14:25:41 +0700

    Все методы привет к вводу данных через параметры.

--- L3T1ADO_NET/Program.cs
+++ L3T1ADO_NET/Program.cs
@@ -30,6 +30,8 @@ namespace L3T1ADO_NET
 
                 InitialData.CreateTables(connection);
 
+                Console.WriteLine();
+
                 //• Вывести общее количество товаров
                 const string sqlProductsCount = "SELECT COUNT(*) FROM dbo.Product";
                 using (var command = new SqlCommand(sqlProductsCount, connection))
@@ -38,36 +40,40 @@ namespace L3T1ADO_NET
 
                     var productsCount = (int)command.ExecuteScalar();
                     Console.WriteLine($"Всего разных продуктов = {productsCount}");
-                    PrintPressKey(true);
+                    PrintPressAnyKey(true);
                 }
 
                 //• Создать некоторую категорию и товар
                 const string printerCategory = "Принтеры";
-                var sqlInsertCategory = $"INSERT INTO dbo.Category(Name) VALUES(N'{printerCategory}')";
+                const string sqlInsertCategory = "INSERT INTO dbo.Category(Name) VALUES(@categoryName)";
 
                 using (var command = new SqlCommand(sqlInsertCategory, connection))
                 {
+                    command.Parameters.Add(new SqlParameter("@categoryName", printerCategory) { SqlDbType = SqlDbType.NVarChar });
                     command.ExecuteNonQuery();
                 }
 
                 int categoryId;
-                var sqlSelectCategoryId = $"SELECT Id FROM dbo.Category WHERE Name='{printerCategory}'";
+                const string sqlSelectCategoryId = @"SELECT Id FROM dbo.Category 
+                                                     WHERE Name=@categoryName";
 
                 using (var command = new SqlCommand(sqlSelectCategoryId, connection))
                 {
+                    command.Parameters.Add(new SqlParameter("@categoryName", printerCategory) { SqlDbType = SqlDbType.NVarChar });
                     categoryId = (int)command.ExecuteScalar();
                 }
 
-                var sqlInsertProduct = $@"INSERT INTO dbo.Product(Name, Price, CategoryId) 
-                                           VALUES
-                                           ('HP Color LaserJet Pro M254dw', 1111,{categoryId}),
-                                           ('Samsung Xpress M2020', 2222,{categoryId}),
-                                           ('Brother HL-L2340DWR', 3333,{categoryId}),
-                                           ('Canon i-SENSYS LBP7018C', 4444,{categoryId}),
-                                           ('Canon i-SENSYS LBP7110Cw', 5555,{categoryId})";
+                const string sqlInsertProduct = @"INSERT INTO dbo.Product(Name, Price, CategoryId) 
+                                                VALUES
+                                                ('HP Color LaserJet Pro M254dw', 1111,@categoryId),
+                                                ('Samsung Xpress M2020', 2222,@categoryId),
+                                                ('Brother HL-L2340DWR', 3333,@categoryId),
+                                                ('Canon i-SENSYS LBP7018C', 4444,@categoryId),
+                                                ('Canon i-SENSYS LBP7110Cw', 5555,@categoryId)";
 
                 using (var command = new SqlCommand(sqlInsertProduct, connection))
                 {
+                    command.Parameters.Add(new SqlParameter("@categoryId", categoryId) { SqlDbType = SqlDbType.Int });
                     command.ExecuteNonQuery();
                 }
 
@@ -93,54 +99,60 @@ namespace L3T1ADO_NET
 
                 Console.WriteLine();
                 Console.WriteLine("-=Таблица Продуктов с новыми позициями=-");
+
                 PrintCategoryProducts(categoryId, connection);
-                PrintPressKey(true);
+                PrintPressAnyKey(true);
 
                 //• Отредактировать некоторый товар 'Samsung Xpress M2020' -> 8888
+                const string sqlCorrectedCategory = @"UPDATE dbo.Product 
+                                                   SET Price=@newPrice 
+                                                   WHERE Name=@correctedProduct";
+
                 const string correctedProduct = "Samsung Xpress M2020";
                 const float newPrice = (float)8888.0;
 
-                var sqlCorrectedCategory = "UPDATE dbo.Product " +
-                                           $"SET Price=CAST({newPrice} AS Decimal(18, 2)) " +
-                                           $"WHERE Name=N'{correctedProduct}'";
-
                 using (var command = new SqlCommand(sqlCorrectedCategory, connection))
                 {
+                    command.Parameters.Add(new SqlParameter("@newPrice", newPrice) { SqlDbType = SqlDbType.Decimal });
+                    command.Parameters.Add(new SqlParameter("@correctedProduct", correctedProduct) { SqlDbType = SqlDbType.NVarChar });
                     command.ExecuteNonQuery();
                 }
 
                 Console.WriteLine("ЗАДАНИЕ 3: Отредактировать некоторый товар");
-                Console.WriteLine($"Редактируем цену {correctedProduct} -> {newPrice}");
                 Console.WriteLine();
+                Console.WriteLine($"Редактируем цену {correctedProduct} -> {newPrice}");
                 Console.WriteLine("-=Таблица Продуктов с отредактированной позицией=-");
 
                 PrintCategoryProducts(categoryId, connection);
-                PrintPressKey(true);
+                PrintPressAnyKey(true);
 
                 //• Удалить некоторый товар
+                const string sqlCutCategory = @"DELETE dbo.Product 
+                                                WHERE Name=@cutProduct";
+
                 const string cutProduct = "Brother HL-L2340DWR";
-                var sqlCutCategory = "DELETE dbo.Product " +
-                                     $"WHERE Name=N'{cutProduct}'";
 
                 using (var command = new SqlCommand(sqlCutCategory, connection))
                 {
+                    command.Parameters.Add(new SqlParameter("@cutProduct", cutProduct) { SqlDbType = SqlDbType.NVarChar });
                     command.ExecuteNonQuery();
                 }
 
-                Console.WriteLine();
                 Console.WriteLine("ЗАДАНИЕ 4: Удалить некоторый товар");
-                Console.WriteLine($"Удаляем позицию {cutProduct}");
+                Console.WriteLine();
 
+                Console.WriteLine($"Удаляем позицию {cutProduct}");
                 Console.WriteLine("-=Таблица Продуктов после удаления позиции=-");
 
                 PrintCategoryProducts(categoryId, connection);
-                PrintPressKey(true);
+                PrintPressAnyKey(true);
 
                 //• Выгрузить весь список товаров вместе с именами категорий через
                 //    reader, и распечатайте все данные в цикле
-                const string sqlShowAllProducts = "SELECT dbo.Product.Id, dbo.Product.Name, dbo.Product.Price, dbo.Category.Name AS 'Категория' " +
-                                                  "FROM dbo.Product LEFT JOIN dbo.Category " +
-                                                  "ON dbo.Product.CategoryId=dbo.Category.Id";
+                const string sqlShowAllProducts = @"SELECT dbo.Product.Id, dbo.Product.Name, dbo.Product.Price, dbo.Category.Name AS 'Категория' 
+                                                  FROM dbo.Product 
+                                                  LEFT JOIN dbo.Category
+                                                  ON dbo.Product.CategoryId=dbo.Category.Id";
 
                 using (var command = new SqlCommand(sqlShowAllProducts, connection))
                 {
@@ -151,49 +163,50 @@ namespace L3T1ADO_NET
                         var columnName3 = reader.GetName(2);
                         var columnName4 = reader.GetName(3);
 
-                        Console.WriteLine();
-                        Console.WriteLine("ЗАДАНИЕ 5: Выгрузить весь список товаров вместе с именами категорий через");
-                        Console.WriteLine("reader, и распечатайте все данные в цикле");
+                        Console.WriteLine("ЗАДАНИЕ 5: Выгрузить весь список товаров вместе с именами категорий через reader, и распечатайте все данные в цикле");
                         Console.WriteLine();
 
                         Console.WriteLine("-=Таблица продуктов через READER=-");
-                        Console.WriteLine($"{columnName1,SpaceId}{columnName2,SpaceName}{columnName3,SpacePrice}{columnName4,SpaceCategory}");
+                        Console.WriteLine($"{columnName1,SpaceId}{columnName2,SpaceName}{columnName3,SpacePrice}  {columnName4,SpaceCategory}");
 
                         while (reader.Read())
                         {
                             Console.WriteLine($"{reader[columnName1],SpaceId}{reader[columnName2],SpaceName}{reader[columnName3],SpacePrice}  {reader[columnName4],SpaceCategory}");
                         }
-                        PrintPressKey(true);
+                        PrintPressAnyKey(true);
                     }
                 }
 
                 //• Выгрузить весь список товаров вместе с именами категорий в
                 //DataSet через SqlDataAdapter, и распечатайте все данные в цикле
-                const string sql = "SELECT * FROM dbo.Product; " +
-                                   "SELECT * FROM dbo.Category";
+                const string sql = @"SELECT * FROM dbo.Product; 
+                                     SELECT * FROM dbo.Category";
 
-                var adapter = new SqlDataAdapter(sql, connection);
-                adapter.TableMappings.Add("Table", "Product");
-                adapter.TableMappings.Add("Table1", "Category");
-
-                var ds = new DataSet();
-                adapter.Fill(ds);
-
-                Console.WriteLine();
-                Console.WriteLine("ЗАДАНИЕ 6: Выгрузить весь список товаров вместе с именами категорий в");
-                Console.WriteLine("DataSet через SqlDataAdapter, и распечатайте все данные в цикле");
-                Console.WriteLine();
-
-                Console.WriteLine("-=Таблица продуктов через SqlDataAdapter=-");
-
-                foreach (DataRow row in ds.Tables["Product"].Rows)
+                using (var adapter = new SqlDataAdapter(sql, connection))
                 {
-                    var categoryName = GetCategoryName((int)row[3], ds.Tables["Category"]);
-                    Console.WriteLine($"{row[0],SpaceId}{row[1],SpaceName}{row[2],SpacePrice}  {categoryName,SpaceCategory}");
+                    adapter.TableMappings.Add("Table", "Product");
+                    adapter.TableMappings.Add("Table1", "Category");
+
+                    using (var ds = new DataSet())
+                    {
+                        adapter.Fill(ds);
+
+                        Console.WriteLine("ЗАДАНИЕ 6: Выгрузить весь список товаров вместе с именами категорий в DataSet через SqlDataAdapter, и распечатайте все данные в цикле");
+                        Console.WriteLine();
+
+                        Console.WriteLine("-=Таблица продуктов через SqlDataAdapter=-");
+
+                        foreach (DataRow row in ds.Tables["Product"].Rows)
+                        {
+                            var categoryName = GetCategoryName((int)row[3], ds.Tables["Category"]);
+                            Console.WriteLine(
+                                $"{row[0],SpaceId}{row[1],SpaceName}{row[2],SpacePrice}  {categoryName,SpaceCategory}");
+                        }
+                    }
                 }
             }
 
-            PrintPressKey(false);
+            PrintPressAnyKey(false);
         }
 
         private static string GetCategoryName(int id, DataTable categoryTable)
@@ -212,22 +225,27 @@ namespace L3T1ADO_NET
             return null;
         }
 
-        private static void PrintPressKey(bool willBeContinue)
+        private static void PrintPressAnyKey(bool willBeContinue)
         {
             Console.WriteLine();
             Console.WriteLine(willBeContinue
                 ? "... press any key."
                 : "КОНЕЦ. Чтобы выйти, нажмите любую клавишу.");
             Console.WriteLine("----------------------------------------");
+            Console.WriteLine();
 
             Console.ReadKey();
         }
 
         private static void PrintCategoryProducts(int categoryId, SqlConnection connection)
         {
-            var sqlShowPrinterCategory = $"SELECT * FROM dbo.Product WHERE CategoryId='{categoryId}'";
+            var sqlShowPrinterCategory = $@"SELECT * FROM dbo.Product 
+                                                  WHERE CategoryId=@categoryId";
+
             using (var command = new SqlCommand(sqlShowPrinterCategory, connection))
             {
+                command.Parameters.Add(new SqlParameter("@categoryId", categoryId) { SqlDbType = SqlDbType.Int });
+
                 using (var reader = command.ExecuteReader())
                 {
                     var columnName1 = reader.GetName(0);
