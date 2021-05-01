5804bdcfc0bdb70a61d9707feb56e19d7ec71d70
9045857 <9045857@mail.ru>
2021-05-01 11:26:15 +0700

    бд удаляется, создается и заполняется из программы

--- L3T1ADO_NET/InitialData.cs
+++ L3T1ADO_NET/InitialData.cs
@@ -1,10 +1,120 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.Data.SqlClient;
+using System;
 
 namespace L3T1ADO_NET
 {
-    class InitialData
+    public class InitialData
     {
+        public static void CreateDb(SqlConnection connection, string dbName)
+        {
+            try
+            {
+                var query = $@"ALTER DATABASE {dbName} 
+                                    SET SINGLE_USER
+                                    WITH ROLLBACK IMMEDIATE;
+
+                                    DROP DATABASE {dbName};";
+
+                using (var cmd = new SqlCommand(query, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("** Database Deleted Successfully");
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+            finally
+            {
+                var query = $"CREATE Database {dbName}";
+                using (var cmd = new SqlCommand(query, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("** Database Created Successfully");
+                }
+            }
+        }
+
+        public static void CreateTables(SqlConnection connection)
+        {
+            var queryCategory = $@"CREATE TABLE[dbo].[Category] (
+                                         Id int IDENTITY(1,1) NOT NULL, 
+                                         Name nvarchar (100) NOT NULL,
+                                         CONSTRAINT PK_Categories PRIMARY KEY(Id))";
+
+            using (var cmd = new SqlCommand(queryCategory, connection))
+            {
+                cmd.ExecuteNonQuery();
+            }
+
+            var queryProduct = $@"CREATE TABLE[dbo].[Product] (
+                                        Id int IDENTITY(1,1) NOT NULL, 
+                                        Name nvarchar (100) NOT NULL,
+                                        Price decimal (18, 2) NOT NULL,
+	                                    CategoryId int NOT NULL,
+                                        CONSTRAINT PK_Products PRIMARY KEY (Id),
+                                        CONSTRAINT FK_Category_Id FOREIGN KEY (CategoryId) REFERENCES dbo.Category (Id))";
+
+            using (var cmd = new SqlCommand(queryProduct, connection))
+            {
+                cmd.ExecuteNonQuery();
+            }
+
+            FillDb(connection);
+            Console.WriteLine("** Database Filled Successfully");
+        }
+
+        public static void FillDb(SqlConnection connection)
+        {
+            var queryCategories = string.Join
+            (
+                Environment.NewLine,
+                "INSERT INTO [dbo].[Category] (Name) ",
+                "VALUES ",
+                "('Верхняя одежда'),",
+                "('Платья и сарафаны'),",
+                "('Свитера и кардиганы'),",
+                "('Блузы и рубашки');"
+            );
+
+            using (var cmd = new SqlCommand(queryCategories, connection))
+            {
+                cmd.ExecuteNonQuery();
+            }
+
+            var queryProduct = string.Join
+            (
+                Environment.NewLine,
+                "INSERT INTO [dbo].[Product] (Name, Price, CategoryId) ",
+                "VALUES ",
+                "('Плащ Violanti', 14497, 1),",
+                "('Плащ Herno', 13484, 1),",
+                "('Бомбер Y-3', 14193, 1),",
+                "('Плащ Emporio Armani', 21225, 1),",
+                "('ПлатьеAVEMOD', 2694, 2),",
+                "('ПлатьеVittoria Vicci', 3459, 2),",
+                "('ПлатьеEmansipe', 2907, 2),",
+                "('ПлатьеAdzhedo', 3704, 2),",
+                "('ПлатьеМодный дом Виктории Тишиной', 1479, 2),",
+                "('ПлатьеПетербургский Швейный Дом', 3817, 2),",
+                "('ДжемперPersona', 4550, 3),",
+                "('СвитшотDesigners Remix', 4482, 3),",
+                "('ДжемперCeline', 29691, 3),",
+                "('КардиганGIORGIO GRATI', 9180, 3),",
+                "('КардиганLuisa Spagnoli', 16884, 3),",
+                "('ТуникаTuzzi', 4850, 4),",
+                "('БлузаRiani', 4070, 4),",
+                "('РубашкаEmporio Armani', 19368, 4),",
+                "('ТуникаAngelo Marani', 6650, 4),",
+                "('БлузаDior', 21195, 4),",
+                "('БлузаMarc Aurel', 2950, 4);"
+            );
+
+            using (var cmd = new SqlCommand(queryProduct, connection))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
     }
 }
--- L3T1ADO_NET/Program.cs
+++ L3T1ADO_NET/Program.cs
@@ -4,7 +4,7 @@ using System.Data;
 
 namespace L3T1ADO_NET
 {
-   internal class Program
+    internal class Program
     {
         private const int SpaceId = -5;
         private const int SpaceName = -35;
@@ -13,179 +13,190 @@ namespace L3T1ADO_NET
 
         private static void Main()
         {
-            const string connectionString = @"Data Source=.;Initial Catalog= ClothingStore;Integrated Security=true;";
+            const string dbName = "L3T1Shop";
 
+            var connectionString = @"Data Source=.;Initial Catalog=master;Integrated Security=true;";
             using (var connection = new SqlConnection(connectionString))
             {
                 connection.Open();
 
-                //• Вывести общее количество товаров
-                const string sqlProductsCount = "SELECT COUNT(*) FROM dbo.Product";
-                using (var command = new SqlCommand(sqlProductsCount, connection))
-                {
-                    Console.WriteLine("ЗАДАНИЕ 1: Вывести общее количество товаров");
+                InitialData.CreateDb(connection, dbName);
+            }
 
-                    var productsCount = (int)command.ExecuteScalar();
-                    Console.WriteLine($"Всего разных продуктов = {productsCount}");
-                    PrintPressKey(true);
-                }
+            connectionString = $@"Data Source=.;Initial Catalog={dbName};Integrated Security=true;";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
 
-                //• Создать некоторую категорию и товар
-                const string printerCategory = "Принтеры";
-                var sqlInsertCategory = $"INSERT INTO dbo.Category(Name) VALUES(N'{printerCategory}')";
+                InitialData.CreateTables(connection);
 
-                using (var command = new SqlCommand(sqlInsertCategory, connection))
-                {
-                    command.ExecuteNonQuery();
-                }
+                ////• Вывести общее количество товаров
+                //const string sqlProductsCount = "SELECT COUNT(*) FROM dbo.Product";
+                //using (var command = new SqlCommand(sqlProductsCount, connection))
+                //{
+                //    Console.WriteLine("ЗАДАНИЕ 1: Вывести общее количество товаров");
 
-                int categoryId;
-                var sqlSelectCategoryId = $"SELECT Id FROM dbo.Category WHERE Name='{printerCategory}'";
+                //    var productsCount = (int)command.ExecuteScalar();
+                //    Console.WriteLine($"Всего разных продуктов = {productsCount}");
+                //    PrintPressKey(true);
+                //}
 
-                using (var command = new SqlCommand(sqlSelectCategoryId, connection))
-                {
-                    categoryId = (int)command.ExecuteScalar();
-                }
+                ////• Создать некоторую категорию и товар
+                //const string printerCategory = "Принтеры";
+                //var sqlInsertCategory = $"INSERT INTO dbo.Category(Name) VALUES(N'{printerCategory}')";
 
-                var printerProducts = new[]
-                {
-                    new Product("HP Color LaserJet Pro M254dw", 1111),
-                    new Product("Samsung Xpress M2020", 2222),
-                    new Product("Brother HL-L2340DWR", 3333),
-                    new Product("Canon i-SENSYS LBP7018C", 4444),
-                    new Product("Canon i-SENSYS LBP7110Cw", 5555)
-                };
+                //using (var command = new SqlCommand(sqlInsertCategory, connection))
+                //{
+                //    command.ExecuteNonQuery();
+                //}
 
-                foreach (var printer in printerProducts)
-                {
-                    var sqlInsertProduct = "INSERT INTO dbo.Product(Name, Price, CategoryId) " +
-                                           $"VALUES(N'{printer.Name}', CAST({printer.Price} AS Decimal(18, 2)),{categoryId})";
-                    using (var command = new SqlCommand(sqlInsertProduct, connection))
-                    {
-                        command.ExecuteNonQuery();
-                    }
-                }
+                //int categoryId;
+                //var sqlSelectCategoryId = $"SELECT Id FROM dbo.Category WHERE Name='{printerCategory}'";
 
-                const string sqlShowCategory = "SELECT * FROM dbo.Category";
-                using (var command = new SqlCommand(sqlShowCategory, connection))
-                {
-                    using (var reader = command.ExecuteReader())
-                    {
-                        var columnName1 = reader.GetName(0);
-                        var columnName2 = reader.GetName(1);
+                //using (var command = new SqlCommand(sqlSelectCategoryId, connection))
+                //{
+                //    categoryId = (int)command.ExecuteScalar();
+                //}
 
-                        Console.WriteLine("ЗАДАНИЕ 2: Создать некоторую категорию и товар");
-                        Console.WriteLine($"Создали категорию '{printerCategory}'");
-                        Console.WriteLine("-=Таблица Категорий=-");
+                //var printerProducts = new[]
+                //{
+                //    new Product("HP Color LaserJet Pro M254dw", 1111),
+                //    new Product("Samsung Xpress M2020", 2222),
+                //    new Product("Brother HL-L2340DWR", 3333),
+                //    new Product("Canon i-SENSYS LBP7018C", 4444),
+                //    new Product("Canon i-SENSYS LBP7110Cw", 5555)
+                //};
 
-                        Console.WriteLine($"{columnName1,SpaceId}{columnName2,SpaceName}");
-                        while (reader.Read())
-                        {
-                            Console.WriteLine($"{reader[columnName1],SpaceId}{reader[columnName2],SpaceName}");
-                        }
-                    }
-                }
+                //foreach (var printer in printerProducts)
+                //{
+                //    var sqlInsertProduct = "INSERT INTO dbo.Product(Name, Price, CategoryId) " +
+                //                           $"VALUES(N'{printer.Name}', CAST({printer.Price} AS Decimal(18, 2)),{categoryId})";
+                //    using (var command = new SqlCommand(sqlInsertProduct, connection))
+                //    {
+                //        command.ExecuteNonQuery();
+                //    }
+                //}
 
-                Console.WriteLine();
-                Console.WriteLine("-=Таблица Продуктов с новыми позициями=-");
-                PrintCategoryProducts(categoryId, connection);
-                PrintPressKey(true);
+                //const string sqlShowCategory = "SELECT * FROM dbo.Category";
+                //using (var command = new SqlCommand(sqlShowCategory, connection))
+                //{
+                //    using (var reader = command.ExecuteReader())
+                //    {
+                //        var columnName1 = reader.GetName(0);
+                //        var columnName2 = reader.GetName(1);
 
-                //• Отредактировать некоторый товар 'Samsung Xpress M2020' -> 8888
-                const string correctedProduct = "Samsung Xpress M2020";
-                const float newPrice = (float)8888.0;
+                //        Console.WriteLine("ЗАДАНИЕ 2: Создать некоторую категорию и товар");
+                //        Console.WriteLine($"Создали категорию '{printerCategory}'");
+                //        Console.WriteLine("-=Таблица Категорий=-");
 
-                var sqlCorrectedCategory = "UPDATE dbo.Product " +
-                                           $"SET Price=CAST({newPrice} AS Decimal(18, 2)) " +
-                                           $"WHERE Name=N'{correctedProduct}'";
+                //        Console.WriteLine($"{columnName1,SpaceId}{columnName2,SpaceName}");
+                //        while (reader.Read())
+                //        {
+                //            Console.WriteLine($"{reader[columnName1],SpaceId}{reader[columnName2],SpaceName}");
+                //        }
+                //    }
+                //}
 
-                using (var command = new SqlCommand(sqlCorrectedCategory, connection))
-                {
-                    command.ExecuteNonQuery();
-                }
+                //Console.WriteLine();
+                //Console.WriteLine("-=Таблица Продуктов с новыми позициями=-");
+                //PrintCategoryProducts(categoryId, connection);
+                //PrintPressKey(true);
 
-                Console.WriteLine("ЗАДАНИЕ 3: Отредактировать некоторый товар");
-                Console.WriteLine($"Редактируем цену {correctedProduct} -> {newPrice}");
-                Console.WriteLine();
-                Console.WriteLine("-=Таблица Продуктов с отредактированной позицией=-");
+                ////• Отредактировать некоторый товар 'Samsung Xpress M2020' -> 8888
+                //const string correctedProduct = "Samsung Xpress M2020";
+                //const float newPrice = (float)8888.0;
 
-                PrintCategoryProducts(categoryId, connection);
-                PrintPressKey(true);
+                //var sqlCorrectedCategory = "UPDATE dbo.Product " +
+                //                           $"SET Price=CAST({newPrice} AS Decimal(18, 2)) " +
+                //                           $"WHERE Name=N'{correctedProduct}'";
 
-                //• Удалить некоторый товар
-                const string cutProduct = "Brother HL-L2340DWR";
-                var sqlCutCategory = "DELETE dbo.Product " +
-                                     $"WHERE Name=N'{cutProduct}'";
+                //using (var command = new SqlCommand(sqlCorrectedCategory, connection))
+                //{
+                //    command.ExecuteNonQuery();
+                //}
 
-                using (var command = new SqlCommand(sqlCutCategory, connection))
-                {
-                    command.ExecuteNonQuery();
-                }
+                //Console.WriteLine("ЗАДАНИЕ 3: Отредактировать некоторый товар");
+                //Console.WriteLine($"Редактируем цену {correctedProduct} -> {newPrice}");
+                //Console.WriteLine();
+                //Console.WriteLine("-=Таблица Продуктов с отредактированной позицией=-");
 
-                Console.WriteLine();
-                Console.WriteLine("ЗАДАНИЕ 4: Удалить некоторый товар");
-                Console.WriteLine($"Удаляем позицию {cutProduct}");
+                //PrintCategoryProducts(categoryId, connection);
+                //PrintPressKey(true);
 
-                Console.WriteLine("-=Таблица Продуктов после удаления позиции=-");
+                ////• Удалить некоторый товар
+                //const string cutProduct = "Brother HL-L2340DWR";
+                //var sqlCutCategory = "DELETE dbo.Product " +
+                //                     $"WHERE Name=N'{cutProduct}'";
 
-                PrintCategoryProducts(categoryId, connection);
-                PrintPressKey(true);
+                //using (var command = new SqlCommand(sqlCutCategory, connection))
+                //{
+                //    command.ExecuteNonQuery();
+                //}
 
-                //• Выгрузить весь список товаров вместе с именами категорий через
-                //    reader, и распечатайте все данные в цикле
-                const string sqlShowAllProducts = "SELECT dbo.Product.Id, dbo.Product.Name, dbo.Product.Price, dbo.Category.Name AS 'Категория' " +
-                                                  "FROM dbo.Product LEFT JOIN dbo.Category " +
-                                                  "ON dbo.Product.CategoryId=dbo.Category.Id";
+                //Console.WriteLine();
+                //Console.WriteLine("ЗАДАНИЕ 4: Удалить некоторый товар");
+                //Console.WriteLine($"Удаляем позицию {cutProduct}");
 
-                using (var command = new SqlCommand(sqlShowAllProducts, connection))
-                {
-                    using (var reader = command.ExecuteReader())
-                    {
-                        var columnName1 = reader.GetName(0);
-                        var columnName2 = reader.GetName(1);
-                        var columnName3 = reader.GetName(2);
-                        var columnName4 = reader.GetName(3);
+                //Console.WriteLine("-=Таблица Продуктов после удаления позиции=-");
 
-                        Console.WriteLine();
-                        Console.WriteLine("ЗАДАНИЕ 5: Выгрузить весь список товаров вместе с именами категорий через");
-                        Console.WriteLine("reader, и распечатайте все данные в цикле");
-                        Console.WriteLine();
+                //PrintCategoryProducts(categoryId, connection);
+                //PrintPressKey(true);
 
-                        Console.WriteLine("-=Таблица продуктов через READER=-");
-                        Console.WriteLine($"{columnName1,SpaceId}{columnName2,SpaceName}{columnName3,SpacePrice}{columnName4,SpaceCategory}");
+                ////• Выгрузить весь список товаров вместе с именами категорий через
+                ////    reader, и распечатайте все данные в цикле
+                //const string sqlShowAllProducts = "SELECT dbo.Product.Id, dbo.Product.Name, dbo.Product.Price, dbo.Category.Name AS 'Категория' " +
+                //                                  "FROM dbo.Product LEFT JOIN dbo.Category " +
+                //                                  "ON dbo.Product.CategoryId=dbo.Category.Id";
 
-                        while (reader.Read())
-                        {
-                            Console.WriteLine($"{reader[columnName1],SpaceId}{reader[columnName2],SpaceName}{reader[columnName3],SpacePrice}  {reader[columnName4],SpaceCategory}");
-                        }
-                        PrintPressKey(true);
-                    }
-                }
+                //using (var command = new SqlCommand(sqlShowAllProducts, connection))
+                //{
+                //    using (var reader = command.ExecuteReader())
+                //    {
+                //        var columnName1 = reader.GetName(0);
+                //        var columnName2 = reader.GetName(1);
+                //        var columnName3 = reader.GetName(2);
+                //        var columnName4 = reader.GetName(3);
 
-                //• Выгрузить весь список товаров вместе с именами категорий в
-                //DataSet через SqlDataAdapter, и распечатайте все данные в цикле
-                const string sql = "SELECT * FROM dbo.Product; " +
-                                   "SELECT * FROM dbo.Category";
+                //        Console.WriteLine();
+                //        Console.WriteLine("ЗАДАНИЕ 5: Выгрузить весь список товаров вместе с именами категорий через");
+                //        Console.WriteLine("reader, и распечатайте все данные в цикле");
+                //        Console.WriteLine();
 
-                var adapter = new SqlDataAdapter(sql, connection);
-                adapter.TableMappings.Add("Table", "Product");
-                adapter.TableMappings.Add("Table1", "Category");
+                //        Console.WriteLine("-=Таблица продуктов через READER=-");
+                //        Console.WriteLine($"{columnName1,SpaceId}{columnName2,SpaceName}{columnName3,SpacePrice}{columnName4,SpaceCategory}");
 
-                var ds = new DataSet();
-                adapter.Fill(ds);
+                //        while (reader.Read())
+                //        {
+                //            Console.WriteLine($"{reader[columnName1],SpaceId}{reader[columnName2],SpaceName}{reader[columnName3],SpacePrice}  {reader[columnName4],SpaceCategory}");
+                //        }
+                //        PrintPressKey(true);
+                //    }
+                //}
 
-                Console.WriteLine();
-                Console.WriteLine("ЗАДАНИЕ 6: Выгрузить весь список товаров вместе с именами категорий в");
-                Console.WriteLine("DataSet через SqlDataAdapter, и распечатайте все данные в цикле");
-                Console.WriteLine();
+                ////• Выгрузить весь список товаров вместе с именами категорий в
+                ////DataSet через SqlDataAdapter, и распечатайте все данные в цикле
+                //const string sql = "SELECT * FROM dbo.Product; " +
+                //                   "SELECT * FROM dbo.Category";
 
-                Console.WriteLine("-=Таблица продуктов через SqlDataAdapter=-");
+                //var adapter = new SqlDataAdapter(sql, connection);
+                //adapter.TableMappings.Add("Table", "Product");
+                //adapter.TableMappings.Add("Table1", "Category");
 
-                foreach (DataRow row in ds.Tables["Product"].Rows)
-                {
-                    var categoryName = GetCategoryName((int)row[3], ds.Tables["Category"]);
-                    Console.WriteLine($"{row[0],SpaceId}{row[1],SpaceName}{row[2],SpacePrice}  {categoryName,SpaceCategory}");
-                }
+                //var ds = new DataSet();
+                //adapter.Fill(ds);
+
+                //Console.WriteLine();
+                //Console.WriteLine("ЗАДАНИЕ 6: Выгрузить весь список товаров вместе с именами категорий в");
+                //Console.WriteLine("DataSet через SqlDataAdapter, и распечатайте все данные в цикле");
+                //Console.WriteLine();
+
+                //Console.WriteLine("-=Таблица продуктов через SqlDataAdapter=-");
+
+                //foreach (DataRow row in ds.Tables["Product"].Rows)
+                //{
+                //    var categoryName = GetCategoryName((int)row[3], ds.Tables["Category"]);
+                //    Console.WriteLine($"{row[0],SpaceId}{row[1],SpaceName}{row[2],SpacePrice}  {categoryName,SpaceCategory}");
+                //}
             }
 
             PrintPressKey(false);
